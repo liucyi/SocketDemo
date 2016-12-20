4c1297de00da96364c73a34ad991bbb272340978
Administrator <Administrator@SC-201611031421>
2016-12-20 16:58:11 +0800

    创建客户端和服务器连接

--- Socket_Server/Program.cs
+++ Socket_Server/Program.cs
@@ -17,21 +17,24 @@ namespace Socket_Server
                 int port = 2000;
                 string host = "127.0.0.1";
                 IPAddress ip = IPAddress.Parse(host);
-                IPEndPoint ipe = new IPEndPoint(ip, port);//把ip和端口转化为IPEndPoint实例
-                Socket c = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//创建一个Socket
-                Console.WriteLine("Conneting...");
-                c.Connect(ipe);//连接到服务器
-                string sendStr = "hello!This is a socket test";
-                byte[] bs = Encoding.ASCII.GetBytes(sendStr);
-                Console.WriteLine("Send Message");
-                c.Send(bs, bs.Length, 0);//发送测试信息
+                IPEndPoint ipe = new IPEndPoint(ip, port);
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//创建一个Socket类
+                s.Bind(ipe);//绑定2000端口
+                s.Listen(0);//开始监听
+                Console.WriteLine("Wait for connect");
+                Socket temp = s.Accept();//为新建连接创建新的Socket。
+                Console.WriteLine("Get a connect");
                 string recvStr = "";
                 byte[] recvBytes = new byte[1024];
                 int bytes;
-                bytes = c.Receive(recvBytes, recvBytes.Length, 0);//从服务器端接受返回信息
+                bytes = temp.Receive(recvBytes, recvBytes.Length, 0);//从客户端接受信息
                 recvStr += Encoding.ASCII.GetString(recvBytes, 0, bytes);
-                Console.WriteLine("Client Get Message:{0}", recvStr);//显示服务器返回信息
-                c.Close();
+                Console.WriteLine("Server Get Message:{0}", recvStr);//把客户端传来的信息显示出来
+                string sendStr = "Ok!Client Send Message Sucessful!";
+                byte[] bs = Encoding.ASCII.GetBytes(sendStr);
+                temp.Send(bs, bs.Length, 0);//返回客户端成功信息
+                temp.Close();
+                s.Close();
             }
             catch (ArgumentNullException e)
             {
